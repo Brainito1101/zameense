bf54c4299160c2d9edfe2a87149700225c7f993e
asha <your_email@example.com>
2026-04-08 18:08:40 +0530

    fixed mime type issue

--- backend/myproject/myproject/urls.py
+++ backend/myproject/myproject/urls.py
@@ -1,18 +1,18 @@
-from django.contrib import admin
 from django.urls import path, include, re_path
+from myapp.views import frontend
+from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from myapp.views import frontend
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myapp.urls')),
 ]
 
-# serve media files
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# serve frontend (React)
+# ✅ Only catch frontend routes (NOT static/assets)
 urlpatterns += [
-    re_path(r'^.*$', frontend),
+    re_path(r'^(?!api|admin|static|media|assets).*$', frontend),
 ]
