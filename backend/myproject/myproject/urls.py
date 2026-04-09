1eea805fb6f0bbe806402dbb4455853a9415bbd2
asha <your_email@example.com>
2026-04-09 16:54:13 +0530

    final fix

--- backend/myproject/myproject/urls.py
+++ backend/myproject/myproject/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
-from django.urls import path, include, re_path
 from myapp.views import frontend
 
 urlpatterns = [
@@ -11,9 +9,10 @@ urlpatterns = [
     path('api/', include('myapp.urls')),
 ]
 
-
-
-urlpatterns += [
-    re_path(r'^(?!api|admin|static|media).*$', frontend),
-]
+# MEDIA (important)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# FRONTEND (must be LAST)
+urlpatterns += [
+    re_path(r'^.*$', frontend),
+]
