f0b237358d8d9b9b838a009c923a0e06f587b6e0
asha <your_email@example.com>
2026-04-09 18:26:20 +0530

    setting file update

--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myproject.myproject.urls'
+ROOT_URLCONF = 'myproject.urls' 
 
 TEMPLATES = [
     {
@@ -76,7 +76,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.myproject.wsgi.application'
+WSGI_APPLICATION = 'myproject.wsgi.application' 
+
 
 
 # Database
