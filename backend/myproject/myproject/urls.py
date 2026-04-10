90cc19ae422ebb06ac4142350bfb17f59a83b5da
asha <your_email@example.com>
2026-04-10 17:07:54 +0530

    Fix static files route order

--- backend/myproject/myproject/urls.py
+++ backend/myproject/myproject/urls.py
@@ -2,17 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import FileResponse
-import os
-
-def serve_index(request, path=''):
-    return FileResponse(open(os.path.join(settings.BASE_DIR, 'templates', 'index.html'), 'rb'), content_type='text/html')
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myapp.urls')),
-    path('', serve_index),
-    path('<path:path>', serve_index),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Add static files BEFORE catch-all routes
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static('/assets/', document_root=settings.BASE_DIR / 'templates' / 'assets')
+
+# Catch-all at the END
+urlpatterns += [
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('<path:url>', TemplateView.as_view(template_name='index.html')),
+]
