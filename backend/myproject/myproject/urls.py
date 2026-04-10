38eb64a9c984e490591f1cfdda719e70d9d3dffb
asha <your_email@example.com>
2026-04-10 16:30:53 +0530

    Fix static files serving for React

--- backend/myproject/myproject/urls.py
+++ backend/myproject/myproject/urls.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import FileResponse
+import os
+
+def serve_index(request, path=''):
+    return FileResponse(open(os.path.join(settings.BASE_DIR, 'templates', 'index.html'), 'rb'), content_type='text/html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('myapp.urls')),  # Your API routes
-    path('', TemplateView.as_view(template_name='index.html')),
-    path('<path:url>', TemplateView.as_view(template_name='index.html')),
+    path('api/', include('myapp.urls')),
+    path('', serve_index),
+    path('<path:path>', serve_index),
 ]
 
-if not settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
